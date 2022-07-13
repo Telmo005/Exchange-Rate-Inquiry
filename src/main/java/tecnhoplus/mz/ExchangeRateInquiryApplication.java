8aaaa0ebd41e9c458ba25dc90dfa8d280daabc44
Telmo Junior <telmo.junior@standardbank.co.mz>
2022-07-13 23:59:57 +0200

    Projecto inicial. Isto na sua forma bruta em spring

---
+++ src/main/java/tecnhoplus/mz/ExchangeRateInquiryApplication.java
@@ -0,0 +1,13 @@
+package tecnhoplus.mz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateInquiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateInquiryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tecnhoplus/mz/ExchangeRateInquiryApplicationTests.java
@@ -0,0 +1,13 @@
+package tecnhoplus.mz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateInquiryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
